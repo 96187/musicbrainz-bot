b82ea9992a6d976246871befaf76d0b094097313
nikki <nikki@rika.musicbrainz.org>
2012-08-26 23:52:02 +0000

    Fixing Ian's syntax error

--- bot.pl
+++ bot.pl
@@ -149,7 +149,7 @@ while (my ($mbid, $comment) = $sth->fetchrow()) {
 			'West Virginia'        => 'WV',
 			Wisconsin              => 'WI',
 			Wyoming                => 'WY'
-		}
+		};
 
 		my $tmp = $comment;
 		$comment =~ s/([,:] )(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|District of Columbia|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming), USA$/$1$states->{$2}, USA/;
